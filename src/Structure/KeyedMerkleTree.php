829469e61d3c17b0efdc4c44cfdacd1cc14e1ac9
Paragon Initiative Enterprises <security@paragonie.com>
2016-04-14 15:02:46 -0400

    Begin work on 2.1.x: Add keyed Merkle Tree structure

--- src/Structure/KeyedMerkleTree.php
+++ src/Structure/KeyedMerkleTree.php
@@ -1,48 +1,25 @@
 <?php
-declare(strict_types=1);
+declare(strict_types = 1);
 namespace ParagonIE\Halite\Structure;
 
-/**
- * An implementation of a Merkle hash tree, built on the BLAKE2b hash function
- * (provided by libsodium)
- */
-class MerkleTree
-{
-    const MERKLE_LEAF =   "\x01";
-    const MERKLE_BRANCH = "\x00";
+use \ParagonIE\Halite\Alerts\InvalidKey;
+use \ParagonIE\Halite\Key;
+use \ParagonIE\Halite\Symmetric\AuthenticationKey;
+use \ParagonIE\Halite\Asymmetric\SignaturePublicKey;
 
-    protected $root = '';
-    protected $nodes = [];
-    
-    /**
-     * Instantiate a Merkle tree
-     * 
-     * @param Node[] $nodes
-     */
-    public function __construct(Node ...$nodes)
-    {
-        $this->nodes = $nodes;
-        $this->root = $this->calculateRoot();
-    }
-    
-    /**
-     * 
-     * @param bool $raw - Do we want a raw string instead of a hex string?
-     * 
-     * @return string
-     */
-    public function getRoot(bool $raw = false): string
-    {
-        return $raw
-            ? $this->root
-            : \Sodium\bin2hex($this->root);
-    }
-    
+/**
+ * Class KeyedMerkleTree
+ * @package ParagonIE\Halite\Structure
+ */
+class KeyedMerkleTree extends MerkleTree
+{
+    private $key = '';
+
     /**
      * Merkle Trees are immutable. Return a replacement with extra nodes.
      *
      * @param Node[] $nodes
-     * @return MerkleTree
+     * @return KeyedMerkleTree
      */
     public function getExpandedTree(Node ...$nodes): MerkleTree
     {
@@ -50,32 +27,40 @@ class MerkleTree
         foreach ($nodes as $node) {
             $thisTree []= $node;
         }
-        return new MerkleTree(...$thisTree);
+        return (new KeyedMerkleTree(...$thisTree))
+            ->setKeyFromString($this->key);
     }
 
     /**
-     * Get the number of actual nodes
+     * Set the key for calculating this Merkle root
      *
-     * @return int
+     * @param string $key
+     * @return self
      */
-    public function getNodeCount(): int
+    public function setKeyFromString(string $key)
     {
-        return \count($this->nodes);
+        $this->key = $key;
+        $this->root = $this->calculateRoot();
+        return $this;
     }
 
     /**
-     * Let's go ahead and round up to the nearest multiple of 2
+     * Set the key for calculating this Merkle root
      *
-     * @param int $inputSize
-     * @return int
+     * @param Key $key AuthenticationKey|SignaturePublicKey
+     *
+     * @throws InvalidKey
+     * @return self
      */
-    public static function getSizeRoundedUp(int $inputSize): int
+    public function setKey(Key $key)
     {
-        $order = 1;
-        while($order < $inputSize) {
-            $order <<= 1;
+        if ($key instanceof AuthenticationKey || $key instanceof SignaturePublicKey) {
+            $this->key = $key->getRawKeyMaterial();
+        } else {
+            throw new InvalidKey('KeyedMerkleTree expects an AuthenticationKey or a SignaturePublicKey.');
         }
-        return $order;
+        $this->root = $this->calculateRoot();
+        return $this;
     }
 
     /**
@@ -108,11 +93,13 @@ class MerkleTree
             for ($i = 0; $i < $order; $i += 2) {
                 if (empty($hash[$i + 1])) {
                     $tmp[$j] = \Sodium\crypto_generichash(
-                        self::MERKLE_BRANCH . $hash[$i] . $hash[$i]
+                        self::MERKLE_BRANCH . $hash[$i] . $hash[$i],
+                        $this->key
                     );
                 } else {
                     $tmp[$j] = \Sodium\crypto_generichash(
-                        self::MERKLE_BRANCH . $hash[$i] . $hash[$i + 1]
+                        self::MERKLE_BRANCH . $hash[$i] . $hash[$i + 1],
+                        $this->key
                     );
                 }
                 ++$j;
---
+++ test/unit/Structure/KeyedMerkleTreeTest.php
@@ -0,0 +1,97 @@
+<?php
+
+use \ParagonIE\Halite\Structure\{
+    KeyedMerkleTree,
+    Node
+};
+
+/**
+ * @backupGlobals disabled
+ * @backupStaticAttributes disabled
+ */
+class KeyedMerkleTreeTest extends PHPUnit_Framework_TestCase
+{
+    public function testExpectedBehavior()
+    {
+        $treeA = new KeyedMerkleTree(
+            new Node('a'),
+            new Node('b'),
+            new Node('c'),
+            new Node('d'),
+            new Node('e')
+        );
+        $treeA->setKeyFromString('');
+        $this->assertSame(
+            '6781891a87aa476454b74dc635c5cdebfc8f887438829ce2e81423f54906c058',
+            $treeA->getRoot()
+        );
+        $treeA->setKeyFromString(
+            \ParagonIE\Halite\Util::raw_hash('' . $treeA->getNodeCount())
+        );
+
+        $this->assertNotEquals(
+            '6781891a87aa476454b74dc635c5cdebfc8f887438829ce2e81423f54906c058',
+            $treeA->getRoot()
+        );
+        $this->assertEquals(
+            '8c53bbdf103a1319fe05bb361c70beacf01c5d55ce70ea900cc4a6e7b95cf524',
+            $treeA->getRoot()
+        );
+
+        $treeB = new KeyedMerkleTree(
+            new Node('a'),
+            new Node('b'),
+            new Node('c'),
+            new Node('d'),
+            new Node('e'),
+            new Node('e'),
+            new Node('e'),
+            new Node('e')
+        );
+        $treeB->setKeyFromString(
+            \ParagonIE\Halite\Util::raw_hash('' . $treeB->getNodeCount())
+        );
+        // Deviation from MerkleTree; if the size of tree is used to key the hash,
+        // then B isn't the same as A
+        $this->assertNotEquals(
+            $treeA->getRoot(),
+            $treeB->getRoot()
+        );
+
+        // Expanded trees inherit the same key
+        $treeC = $treeA->getExpandedTree(
+            new Node('e'),
+            new Node('e'),
+            new Node('e')
+        );
+        $this->assertEquals(
+            $treeA->getRoot(),
+            $treeC->getRoot()
+        );
+        // We're setting the key this time, so it should differ.
+        $treeD = $treeA->getExpandedTree(
+            new Node('e'),
+            new Node('e'),
+            new Node('e')
+        );
+        $treeD->setKeyFromString(
+            \ParagonIE\Halite\Util::raw_hash('' . $treeD->getNodeCount())
+        );
+
+        $this->assertNotEquals(
+            $treeA->getRoot(),
+            $treeD->getRoot()
+        );
+
+        // This should fail
+        $treeE = $treeA->getExpandedTree(
+            new Node('f'),
+            new Node('e'),
+            new Node('e')
+        );
+        $this->assertNotEquals(
+            $treeA->getRoot(),
+            $treeE->getRoot()
+        );
+    }
+}
